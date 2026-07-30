fd199e742e87cce93ff8cf9ae3520504517e0e84
Righteousness Ude <righteousnessude@gmail.com>
2026-07-30 10:07:37 +0100

    Update image assignment to use uploaded file name

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
-    image = uploaded_file
+    image = uploaded_file.name
 
     col1, col2 = st.columns([1, 1], gap="large")
 
